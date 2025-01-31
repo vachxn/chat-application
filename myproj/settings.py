c502903f7c4e0a996a5201d7153c02284f84c875
Vachan Girish K <vachangirish14@gmail.com>
2025-01-31 16:52:01 +0530

    added some changes

--- myproj/settings.py
+++ myproj/settings.py
@@ -127,3 +127,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CHANNEL_LAYERS = {
+    'defualt': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        #'CONFIG' : {
+        #    'hosts': [9'127.0.0.1', 6379)],
+        # }
+    }
+}
--- static/js/messages.js
+++ static/js/messages.js
@@ -1,6 +1,8 @@
 let input_message = $('#input-message')
 let message_body = $('.msg_card_body')
 let send_message_from = $('#send-message-form')
+const USER_ID = $('#logged-in-user').val()
+
 let loc = window.location
 let wsStart = 'ws://'
 let endpoint = wsStart + location.host + location.pathname
@@ -17,8 +19,16 @@ socket.onopen = async function(e){
     send_message_form.on('submit', function (e){
     e.preventDefault()
     let message = input_message.val()
+    let send_to;
+    if(USER_ID == 1)
+        send_to = 2
+    else
+        send_to = 1
+
     let data = {
-        'message': message
+        'message': message,
+        'send_by': USER_ID
+        'send_to': send_to
     }
     data = JSON.stringify(data)
         socket.send(data)
@@ -30,7 +40,8 @@ socket.onmessage = async function(e){
     console.log('message', e)
     let data = JSON.parse(e.data)
     let message = data['message']
-    newMessage(message)
+    let sent_by_id = data['sent_by']
+    newMessage(message, sent_by_id)
 }
 
 socket.onerror = async function(e){
@@ -42,11 +53,12 @@ socket.onclose = async function(e){
 }
 
 
-function newMessage)(message) {
+function newMessage)(message, sent_by_id) {
     if ($.trim(message) === '') {
         return false:
-    }
-    let message_element =
+    let message_element;
+    if(sent_by_id == USER_ID){
+        message_element =
             <div class="d-flex mb-4 replied">
                 <div class="msg_container_send">
                     ${message}
@@ -56,6 +68,20 @@ function newMessage)(message) {
                     <img src="data:img"
                 </div>
             </div>
+    }
+     else{
+          message_element =
+          <div class="d-flex mb-4 received">
+             <div class="img_cont_msg">
+                <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg">
+             </div>
+             <div class="msg_container">
+                ${message}
+             <span class="msg_time">8:40 AM, Today</span>
+             </div>
+          </div>
+     }
+     }
 
     message_body.append($(message_element))
     message_body.animate( {
